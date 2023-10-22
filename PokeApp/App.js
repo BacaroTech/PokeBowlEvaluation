1cc8082aa63a49662fff2c1743b037455ba16ca4
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-10-22 08:56:51 +0200

    aggiunto tailwind al progetto

--- PokeApp/App.js
+++ PokeApp/App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import tw from 'twrnc'
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`bg-black w-100`}>
+      <Text style={tw`bg-white w-100`}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });

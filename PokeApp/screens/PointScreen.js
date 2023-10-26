d41da9f06de0747140acb4d47a56e2333a3648d0
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2023-10-26 19:59:00 +0200

    capito come funzionano le chiamate api su react native con un esempio di pokeapi

--- PokeApp/screens/PointScreen.js
+++ PokeApp/screens/PointScreen.js
@@ -1,16 +1,35 @@
 import tw from 'twrnc'
-import {React, createElement } from 'react';
-import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import {React, createElement, useEffect, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import { Dimensions } from "react-native";
 import { useRoute } from "@react-navigation/native"
+import axios from 'axios';
 
 var width = Dimensions.get('window').width; //full width
 var height = Dimensions.get('window').height; //full height
 
 export default function Point() {
     const route = useRoute()
-    const paramState = route.params?.paramState
-    console.log(paramState)
+    const paramState = route.params?.paramState //param mpassed by prev form
+
+    const [inputText, setInputText] = useState('');
+    const [response, setResponse] = useState('');
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+    const sendMessage = async () => {
+        try {
+        const requestData = {
+            prompt: inputText,
+            max_tokens: 50, // Puoi regolare questa opzione per controllare la lunghezza della risposta.
+        };
+        const response = await axios.get(apiUrl, requestData);
+        console.log(response)
+        setResponse(response.data.base_experience);
+        } catch (error) {
+            console.error('Errore nella chiamata API:', error);
+        }
+    };
+
     return (
         <View style={styles.box}>
             <View style={tw`flex flex-col mt-20 px-6 lg:px-8`}>
@@ -18,8 +37,15 @@ export default function Point() {
                     <Text style={tw`text-center text-5xl font-bold leading-9 tracking-tight text-gray-900 leading-relaxed`}>Punteggio</Text>
                 </View>
             </View> 
+            <TextInput
+            placeholder="Inserisci il tuo messaggio"
+            value={inputText}
+            onChangeText={(text) => setInputText(text)}
+            />
+            <Button title="Invia" onPress={sendMessage} />
+            {response && <Text>Risposta: {response}</Text>}
         </View>
-    )
+    ) 
 }
 
 const styles = StyleSheet.create({
@@ -33,6 +59,7 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         color: "black",
         fontSize: "1.25rem",
-        height: 'auto'
+        height: 'auto',
+        minHeight: height
     }
 });
